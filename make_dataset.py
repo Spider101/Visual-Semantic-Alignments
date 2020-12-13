85ab1d1732688bf9a623c418c7659ccb5c10dda4
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2020-12-13 14:33:48 +0530

    add code to rename data file after unpacking it from archive

--- make_dataset.py
+++ make_dataset.py
@@ -1,10 +1,10 @@
 import argparse
 import requests
 import shutil
-from os import path, remove, listdir
+from os import path, remove, listdir, rename
 from tqdm import tqdm
 
-from config.resources import path_to_external_data
+from config.resources import path_to_external_data, VG_IMG_FNAME, VG_REG_FNAME, PARA_FNAME
 
 def download_from_url(url, dataset_name, path_to_save_location):
     print('\nStarting download of {} zip file...'.format(dataset_name))
@@ -20,11 +20,18 @@ def download_from_url(url, dataset_name, path_to_save_location):
             fd.write(chunk)
     progress_bar.close()
 
-def unpack_archive(archive_path, dataset_name, path_to_save_location):
+def unpack_archive(archive_path, dataset_name, path_to_save_dir, renamed_fname):
     # unpack and then remove the zip file
     print('\nUnzipping and then removing archive file for {} metadata...'.format(dataset_name))
-    shutil.unpack_archive(archive_path, path_to_save_location)
+    shutil.unpack_archive(archive_path, path_to_save_dir)
     remove(archive_path)
+
+    # rename the extracted file to something project specific
+    original_fname = path.basename(archive_path).split('.zip')[0]
+    print('\nRenaming extracted file from {} to {}'.format(original_fname, renamed_fname))
+    original_fpath = path.join(path_to_save_dir, original_fname)
+    updated_fpath = path.join(path_to_save_dir, renamed_fname)
+    rename(original_fpath, updated_fpath)
 
     print('Downloaded and unpacked data for Region Description Metadata')
 
@@ -41,7 +48,7 @@ def make_image_paragraphs_dataset():
     # build image paragraphs metadata dataset
     image_paragraphs_metadata_name = 'Image Paragraphs Metadata'
     download_from_url(image_para_metadata_url, image_paragraphs_metadata_name, path_to_save_image_para_metadata)
-    unpack_archive(path_to_save_image_para_metadata, image_paragraphs_metadata_name, path_to_external_data)
+    unpack_archive(path_to_save_image_para_metadata, image_paragraphs_metadata_name, path_to_external_data, PARA_FNAME)
 
     show_datasets_in_folder(path_to_external_data)
 
@@ -49,18 +56,18 @@ def make_visual_genome_dataset():
     region_desc_metadata_url = input("Enter URL for downloading region description metadata: ")
     image_metadata_url = input("Enter URL for downloading image metadata: ")
 
-    path_to_save_region_desc_metadata = path.join(path_to_external_data, 'region_desc_metadata.json.zip')
-    path_to_save_image_metadata = path.join(path_to_external_data, 'image_metadata.json.zip')
+    path_to_save_region_desc_metadata = path.join(path_to_external_data, 'region_descriptions.json.zip')
+    path_to_save_image_metadata = path.join(path_to_external_data, 'image_data.json.zip')
 
     # build region descriptions metadata dataset
     region_desc_metadata_name = 'Region Description Metadata'
     download_from_url(region_desc_metadata_url, region_desc_metadata_name, path_to_save_region_desc_metadata)
-    unpack_archive(path_to_save_region_desc_metadata, region_desc_metadata_name, path_to_external_data)
+    unpack_archive(path_to_save_region_desc_metadata, region_desc_metadata_name, path_to_external_data, VG_REG_FNAME)
 
     # build image metadata dataset
     image_metadata_name = 'Image Metadata'
     download_from_url(image_metadata_url, image_metadata_name, path_to_save_image_metadata)
-    unpack_archive(path_to_save_image_metadata, image_metadata_name, path_to_external_data)
+    unpack_archive(path_to_save_image_metadata, image_metadata_name, path_to_external_data, VG_IMG_FNAME)
 
     show_datasets_in_folder(path_to_external_data)
 
