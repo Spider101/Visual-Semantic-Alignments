b15b170efceca4a47dd3d4635a06261c5327e893
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2020-12-13 13:15:35 +0530

    refactor to move common code to util methods

--- make_dataset.py
+++ make_dataset.py
@@ -6,6 +6,27 @@ from tqdm import tqdm
 
 from config.resources import path_to_external_data
 
+def download_from_url(url, dataset_name, path_to_save_location):
+    print('\nStarting download of {} zip file...'.format(dataset_name))
+    response = requests.get(url, stream=True)
+    total_size_in_bytes= int(response.headers.get('content-length', 0))
+    block_size = 1024 #1 Kibibyte
+    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+
+    # write the response to the region desc metadata zip file in local data folder
+    with open(path_to_save_location, 'wb') as fd:
+        for chunk in response.iter_content(chunk_size=block_size):
+            progress_bar.update(len(chunk))
+            fd.write(chunk)
+    progress_bar.close()
+
+def unpack_archive(archive_path, dataset_name, path_to_save_location):
+    # unpack and then remove the zip file
+    print('\nUnzipping and then removing archive file for {} metadata...'.format(dataset_name))
+    shutil.unpack_archive(archive_path, path_to_save_location)
+    remove(archive_path)
+
+    print('Downloaded and unpacked data for Region Description Metadata')
 
 def make_visual_genome_dataset():
     region_desc_metadata_url = input("Enter URL for downloading region description metadata: ")
@@ -15,44 +36,17 @@ def make_visual_genome_dataset():
     path_to_save_region_desc_metadata = path.join(path_to_external_data, 'region_desc_metadata.json.zip')
     path_to_save_image_metadata = path.join(path_to_external_data, 'image_metadata.json.zip')
 
-    print('\nStarting download of region descriptions metadata zip file...')
-    region_metadata_response = requests.get(region_desc_metadata_url, stream=True)
-    total_size_in_bytes= int(region_metadata_response.headers.get('content-length', 0))
-    block_size = 1024 #1 Kibibyte
-    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+    # build region descriptions metadata dataset
+    region_desc_metadata_dataset_name = 'Region Description Metadata'
+    download_from_url(region_desc_metadata_url, region_desc_metadata_dataset_name, path_to_save_region_desc_metadata)
+    unpack_archive(path_to_save_region_desc_metadata, region_desc_metadata_dataset_name, path_to_save_data)
 
-    # write the response to the region desc metadata zip file in local data folder
-    with open(path_to_save_region_desc_metadata, 'wb') as fd:
-        for chunk in region_metadata_response.iter_content(chunk_size=block_size):
-            progress_bar.update(len(chunk))
-            fd.write(chunk)
-    progress_bar.close()
+    # build image metadata dataset
+    image_metadata_dataset_name = 'Image Metadata'
+    download_from_url(image_metadata_url, image_metadata_dataset_name, path_to_save_image_metadata)
+    unpack_archive(path_to_save_image_metadata, image_metadata_dataset_name, path_to_save_data)
 
-    # unpack and then remove the zip file
-    print('\nUnzipping and then removing archive file for region description metadata...')
-    shutil.unpack_archive(path_to_save_region_desc_metadata, path_to_save_data)
-    remove(path_to_save_region_desc_metadata)
-
-    print('Downloaded and unpacked data for Region Description Metadata')
-
-    print('\nStarting download of image metadata zip file...')
-    image_metadata_response = requests.get(image_metadata_url)
-    total_size_in_bytes= int(image_metadata_response.headers.get('content-length', 0))
-    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
-
-    # write the response to the region desc metadata zip file in local data folder
-    with open(path_to_save_image_metadata, 'wb') as fd:
-        for chunk in image_metadata_response.iter_content(chunk_size=block_size):
-            progress_bar.update(len(chunk))
-            fd.write(chunk)
-    progress_bar.close()
-
-    # unpack and then remove the zip file
-    print('\nUnzipping and then removing archive file for image metadata...')
-    shutil.unpack_archive(path_to_save_image_metadata, path_to_save_data)
-    remove(path_to_save_image_metadata)
-
-    print('\nDownloaded and unpacked data for Image Metadata in the following locations:')
+    print('\nDownloaded and unpacked data for Region Description and Image Metadata in the following locations:')
     for idx, file_path in enumerate(listdir(path_to_save_data)):
         print('{}. {}'.format(idx + 1, path.join(path_to_save_data, file_path)))
 
